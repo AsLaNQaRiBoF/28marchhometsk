27edfa95246eb55420d5c4086cec7be9a9906eed
aslan <aslanqaribov7@gmail.com>
2023-03-28 13:05:22 +0400

    commit

---
+++ hometask28march/hometask28march/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+
+        #region MyRegion
+        string[] aylar_otuzbir = { "Yanvar", "Mart", "May", "Iyul", "Avqust", "Oktyabr", "Dekabr" };
+        string[] aylar_otuz = { "Aprel", "Iyun", "Sentyabr", "Noyabr" };
+        string[] fevral = { "fevral" };
+        string input = Console.ReadLine();
+        foreach (var item in fevral)
+        {
+            if (input==item)
+            {
+                Console.WriteLine("ayin 28 ve ya 29 gunu var");
+            }
+        }
+
+        foreach (var item in aylar_otuzbir)
+        {
+            if (input == item)
+            {
+                Console.WriteLine("ayin 31 gunu var");
+
+            }
+
+        }
+        foreach (var item in aylar_otuz)
+        {
+            if (input == item)
+            {
+                Console.WriteLine("ayin 30 gunu var");
+
+            }
+
+        }
+    }
+}
+#endregion
+
+
